be68e349a29c859dc9da24dc994f21594d7320b6
Roman <roman.markanych@gmail.com>
2022-08-05 18:56:44 +0300

    added 1 task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,15 @@
 const allCategories = document.querySelectorAll('.item');
 console.log(`Number of categories: ${allCategories.length}`);
 
-const categoriesName = document.querySelectorAll('li.item h2');
-const categoriesUl = document.querySelectorAll('li.item ul');
-categoriesName.forEach(name => console.log(`Category: ${name.textContent}`));
+allCategories.forEach(name => {
+  console.log(`Category: ${name.firstElementChild.textContent}`);
+  console.log(`Category: ${name.lastElementChild.children.length}`);
+});
+
+// const categoriesName = document.querySelectorAll('li.item h2');
+
+// const categoriesUl = document.querySelectorAll('li.item ul');
+
+// categoriesName.forEach(name => console.log(`Category: ${name.textContent}`));
+
+// categoriesUl.forEach(name => console.log(`Category: ${name.children.length}`));
