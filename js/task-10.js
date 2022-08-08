4cac7777929e4258b0aa8aa38e803589b43b6863
Roman <roman.markanych@gmail.com>
2022-08-08 21:35:00 +0300

    add task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
+const controls = document.querySelector('#controls');
+const inputValue = document.querySelector('input[type="number"]');
+const btnCreate = controls.querySelector('button[data-create]');
+const btnDestroy = controls.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+let divWidth = 30;
+let divHeight = 30;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+btnCreate.addEventListener('click', createBoxes);
+
+function createBoxes() {
+  destroyBoxes();
+  for (let i = 1; i <= inputValue.value; i++) {
+    boxes.insertAdjacentHTML(
+      'beforeend',
+      `<div class="div-create" style="
+      background-color:${getRandomHexColor()};
+          width:${(divWidth += 10)}px;
+          height:${(divHeight += 10)}px"
+      ></div>`
+    );
+  }
+  inputValue.value = '';
+}
+
+btnDestroy.addEventListener('click', () => {
+  inputValue.value = '';
+  destroyBoxes();
+});
+
+function destroyBoxes() {
+  boxes.textContent = '';
+  divWidth = 20;
+  divHeight = 20;
 }
